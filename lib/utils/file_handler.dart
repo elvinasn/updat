7ec29879c678ec9fbea55deb17e6aea694a7ed17
Elvinas <elviz.nejuz@gmail.com>
2024-08-20 18:41:34 +0300

    bump archive version

--- lib/utils/file_handler.dart
+++ lib/utils/file_handler.dart
@@ -49,15 +49,13 @@ Future<void> openInstaller(File file, String appName) async {
   if (file.existsSync()) {
     if (file.path.endsWith("zip")) {
       final outDir = Directory(p.join(p.dirname(file.path), appName));
-      file = File(
-          outDir.listSync().firstWhere((e) {
-            if (Platform.isWindows) {
-              return e.path.endsWith("exe");
-            } else {
-              return true;
-            }
-          }).path
-      );
+      file = File(outDir.listSync().firstWhere((e) {
+        if (Platform.isWindows) {
+          return e.path.endsWith("exe");
+        } else {
+          return true;
+        }
+      }).path);
     }
     await openUri(Uri(path: file.absolute.path, scheme: 'file'));
   } else {
